4c2ae2e023c4f826c65117d0c4d365a7f2d88ce8
mcnight89 <mc.night89@gmail.com>
2023-01-26 15:57:39 +0300

    1

--- skills.py
+++ skills.py
@@ -1,16 +1,20 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from unit import BaseUnit
+
+# from typing import TYPE_CHECKING
+
+# if TYPE_CHECKING:
+# from unit import BaseUnit
 
 class Skill(ABC):
     """
     Базовый класс умения
     """
-    user = None
-    target = None
+
+    def __init__(self):
+        self.user = None
+        self.target = None
 
     @property
     @abstractmethod
@@ -34,7 +38,7 @@ class Skill(ABC):
     def _is_stamina_enough(self):
         return self.user.stamina > self.stamina
 
-    def use(self, user: BaseUnit, target: BaseUnit) -> str:
+    def use(self, user, target) -> str:
         """
         Проверка, достаточно ли выносливости у игрока для применения умения.
         Для вызова скилла везде используем просто use
@@ -47,9 +51,9 @@ class Skill(ABC):
 
 
 class FuryPunch(Skill):
-    name = ...
-    stamina = ...
-    damage = ...
+    name = "Свирепый пинок"
+    stamina = 6
+    damage = 12
 
     def skill_effect(self):
         # TODO логика использования скилла -> return str
@@ -57,12 +61,19 @@ class FuryPunch(Skill):
         # TODO именно здесь происходит уменшение стамины у игрока применяющего умение и
         # TODO уменьшение здоровья цели.
         # TODO результат применения возвращаем строкой
-        pass
+        self.user.stamina -= self.stamina
+        self.target.stamina -= self.stamina
+
+        return f"{self.user.name} использует {self.name} и наносит {self.damage} урона сопернику."
+
 
 class HardShot(Skill):
-    name = ...
-    stamina = ...
-    damage = ...
+    name = "Мощный укол"
+    stamina = 5
+    damage = 15
 
     def skill_effect(self):
-        pass
+        self.user.stamina -= self.stamina
+        self.target.stamina -= self.stamina
+
+        return f"{self.user.name} использует {self.name} и наносит {self.damage} урона сопернику."
